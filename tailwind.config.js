a101b2b3d71c008387721e38ca6df21eaffcc275
ka-few <njorovista@gmail.com>
2026-01-02 18:08:14 +0300

    update configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  // NOTE: This is the most common point of failure.
+  // Make sure it covers your 'app' folder and any 'components' folder.
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./App.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
